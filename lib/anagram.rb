fad68a7933d2fda68384ade0252caea7e0834b23
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-13 12:12:44 -0800

    doing research

---
+++ lib/anagram.rb
@@ -0,0 +1,11 @@
+class Anagram
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
+  end
+
+  def anagram_check()
+    input_one_array = @input1.
+
+
+end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,6 +3,8 @@ require('pry')
 require('anagram')
 
 describe('Anagram#anagram_check') do
-  anagram = Anagram.new()
-  expect(anagram.anagram_check('ruby','bury')).to(eq('These words are anagrams.'))
+  it('checks to see if the words are anagrams') do
+    anagram = Anagram.new()
+    expect(anagram.anagram_check('ruby','bury')).to(eq('These words are anagrams.'))
+  end
 end
