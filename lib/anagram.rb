aa4c754733f5cf80d72d18c2ab54b37213ed5949
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-13 12:29:26 -0800

    adds passing test to see if given word is a anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,16 @@
 class Anagram
   def initialize(input1, input2)
-    @input1 = input1
-    @input2 = input2
+    @input1 = input1.downcase.split('').sort
+    @input2 = input2.downcase.split('').sort
   end
 
   def anagram_check()
-    input_one_array = @input1.
+    if @input1 == @input2
+      return "These words are anagrams."
+    end
+  
+
+end
 
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,7 +4,7 @@ require('anagram')
 
 describe('Anagram#anagram_check') do
   it('checks to see if the words are anagrams') do
-    anagram = Anagram.new()
-    expect(anagram.anagram_check('ruby','bury')).to(eq('These words are anagrams.'))
+    anagram = Anagram.new('ruby','bury')
+    expect(anagram.anagram_check()).to(eq('These words are anagrams.'))
   end
 end
