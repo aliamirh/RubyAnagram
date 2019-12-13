94c405cd30ed6aa4ab962d1c06eb2ea8c43ae891
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-13 13:44:46 -0800

    passes second test to see if its not a anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,15 @@
 class Anagram
   def initialize(input1, input2)
-    @input1 = input1.downcase.split('').sort
-    @input2 = input2.downcase.split('').sort
+    @input1 = input1.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
+    @input2 = input2.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
   end
 
   def anagram_check()
     if @input1 == @input2
       return "These words are anagrams."
-    
+    else
+      return "Sorry, these are not anagrams."
+    end
   end
+  
 end
