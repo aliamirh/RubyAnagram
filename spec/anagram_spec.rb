839297cad962aca805eb712f131399f78ffa5e37
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-13 16:39:55 -0800

    change some spelling errors

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -15,15 +15,15 @@ describe('Anagram#anagram_check') do
     anagram = Anagram.new('RuBy','bury')
     expect(anagram.anagram_check()).to(eq('These words are anagrams.'))
   end
-  it ('checks to see of word has a vowel') do
+  it ('checks to see if word has a vowel') do
     anagram = Anagram.new('ruby','bury')
     expect(anagram.anagram_vowel_check()).to(eq('has vowels'))
   end
-  it ('checks to see of word doesnt have a vowel') do
+  it ('checks to see if word doesnt have a vowel') do
     anagram = Anagram.new('rtby','btry')
     expect(anagram.anagram_vowel_check()).to(eq('no vowels'))
   end
-  it ('checks to see of word doesnt have a vowel') do
+  it ('checks to see if the inputs are an antigram') do
     anagram = Anagram.new('runy','runy')
     expect(anagram.antigram_check()).to(eq('this is an antigram'))
   end
