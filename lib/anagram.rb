aa5278cc179f2169ebc35bd69143ae5642ab1b80
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-12-13 16:02:06 -0800

    add vowel check test and method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,12 @@
 class Anagram
   def initialize(input1, input2)
-    @input1 = input1
-    @input2 = input2
+    @input1 = input1.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
+    @input2 = input2.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
   end
 
   def anagram_check()
-    input1 = @input1.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
-    input2 = @input2.downcase.gsub(/[^a-z0-9]/i, '').split('').sort
-    if input1 == input2
+
+    if @input1 == @input2
       return "These words are anagrams."
     else
       return "Sorry, these are not anagrams."
@@ -17,10 +16,10 @@ class Anagram
     # input1 = @input1.scan(/[^aeiou]/)
     # input2 = @input2.scan(/[^aeiou]/)
 
-    if @input1.include?(/[aeiou]/) || @input2.include?(/[^aeiou]/)
-      return "no vowels"
+    if @input1.any?(/[aeiou]/) || @input2.any?(/[aeiou]/)
+      return "has vowels"
     else
-      return 'has vowels'
+      return 'no vowels'
     end
   end
 end
